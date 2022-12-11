c02bdf1d23629e8fc5a4d9554b9bfd736718208c
Mariano <marianomacat@gmail.com>
2022-12-11 10:04:11 -0300

    label

--- main.py
+++ main.py
@@ -20,13 +20,17 @@ def plot_ticker(ticker_name):
 
         fig = px.line(df, x='Date', y='High', title='Time Series with Range Slider and Selectors')
 
+        fig.update_layout(
+            title= ' Live Share Price:',
+            yaxis_title='Stock Price (USD per Shares)')  
+
         fig.update_xaxes(
             rangeslider_visible=True,
                 rangeselector=dict(
                     buttons=list([
-                        dict(count=1, label="1m", step="month", stepmode="backward"),
-                        dict(count=6, label="6m", step="month", stepmode="backward"),
-                        dict(count=1, label="YTD", step="year", stepmode="todate"),
+                        dict(count=1, label="15m", step="minute", stepmode="backward"),
+                        dict(count=6, label="1m", step="month", stepmode="backward"),
+                        dict(count=1, label="6m", step="month", stepmode="todate"),
                         dict(count=1, label="1y", step="year", stepmode="backward"),
                         dict(step="all")
                     ])
