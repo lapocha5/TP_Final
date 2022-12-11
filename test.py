cea8a9662e09782fa743fc01aa364c9cad1bd5e2
Mariano <marianomacat@gmail.com>
2022-12-11 09:35:32 -0300

    Cambio Gráfico

--- test.py
+++ test.py
@@ -2,16 +2,23 @@ import sqlite3
 import pandas as pd
 import yfinance as yf
 import plotly.graph_objects as go
-
-conn = sqlite3.connect("ticker_data.db")
+import plotly.express as px
+import pandas as pd
 
 data_to_plot = pd.read_sql("SELECT Date, Open, High, Low, Close FROM AAPL", conn)
-fig = go.Figure(data=[go.Candlestick(x=data_to_plot['Date'],
-                open=data_to_plot['Open'],
-                high=data_to_plot['High'],
-                low=data_to_plot['Low'],
-                close=data_to_plot['Close'])])
 
+fig = px.line(df, x='Date', y='AAPL.High', title='Time Series with Range Slider and Selectors')
+
+fig.update_xaxes(
+    rangeslider_visible=True,
+    rangeselector=dict(
+        buttons=list([
+            dict(count=1, label="1m", step="month", stepmode="backward"),
+            dict(count=6, label="6m", step="month", stepmode="backward"),
+            dict(count=1, label="YTD", step="year", stepmode="todate"),
+            dict(count=1, label="1y", step="year", stepmode="backward"),
+            dict(step="all")
+        ])
+    )
+)
 fig.show()
-
-
