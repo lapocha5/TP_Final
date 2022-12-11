5cd0399d4e718e5704665e39b6e96a48a4909a2c
Mariano <marianomacat@gmail.com>
2022-12-11 10:26:47 -0300

    Nueva mod gráfica

--- main.py
+++ main.py
@@ -18,10 +18,15 @@ def plot_ticker(ticker_name):
     if error_code == 0:
         df = pd.read_sql("SELECT Date, Open, High, Low, Close FROM AAPL", conn)
 
-        fig = px.line(df, x='Date', y='High', title='Time Series with Range Slider and Selectors')
+        fig=go.Figure()
+
+        fig.add_trace(go.Candlestick(x=df.index,
+                open=df['Open'],
+                high=df['High'],
+                low=df['Low'],
+                close=df['Close'], name = 'market data'))
 
         fig.update_layout(
-            title= ' Live Share Price:',
             yaxis_title='Stock Price (USD per Shares)')  
 
         fig.update_xaxes(
@@ -29,8 +34,8 @@ def plot_ticker(ticker_name):
                 rangeselector=dict(
                     buttons=list([
                         dict(count=1, label="15m", step="minute", stepmode="backward"),
-                        dict(count=6, label="1m", step="month", stepmode="backward"),
-                        dict(count=1, label="6m", step="month", stepmode="todate"),
+                        dict(count=6, label="6m", step="month", stepmode="backward"),
+                        dict(count=1, label="YTD", step="year", stepmode="todate"),
                         dict(count=1, label="1y", step="year", stepmode="backward"),
                         dict(step="all")
                     ])
