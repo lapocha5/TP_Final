4d95a73d84e5f1bb3b3949593a9cf1f8df979732
Mariano <marianomacat@gmail.com>
2022-12-18 06:56:02 -0300

    Creado

---
+++ Errores.py
@@ -0,0 +1,7 @@
+def error_handling(error_code):
+    if error_code == 1:
+        print("El Ticker es invalido, favor de verificar e intentar nuevamente.")
+    elif error_code == 2:
+        print("El Ticker es invalido, favor de verificar e intentar nuevamente.")
+    elif error_code == 3:
+        print("El rango elegido ha excedido las capacidades de este sistema, por favor ingrese un rango más corto.")
